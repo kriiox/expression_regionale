8fd1ef2b9162525071d22c09fb0875b5b7fb68cc
Thibaut BISOGNANI <thibautbisog11@gmail.com>
2021-02-25 20:22:02 +0100

    Modification de la BD + ajout d'autres procédures

--- BD/bdd_expression.sql
+++ BD/bdd_expression.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le : jeu. 11 fév. 2021 à 20:57
+-- Généré le : jeu. 25 fév. 2021 à 19:17
 -- Version du serveur :  5.7.31
 -- Version de PHP : 7.3.21
 
@@ -28,28 +28,42 @@ DELIMITER $$
 -- Procédures
 --
 DROP PROCEDURE IF EXISTS `ajoutExpression`$$
-CREATE DEFINER=`root`@`localhost` PROCEDURE `ajoutExpression` (`p_mot` VARCHAR(255), `p_numGroupe` INT, `p_numRegion` INT)  INSERT INTO expression (mots, num_Groupe, num_Region) 
-    VALUES (p_mot, p_numGroupe, p_numRegion)$$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `ajoutExpression` (`p_mot` VARCHAR(255), `p_numGroupe` INT, `p_numRegion` INT)  BEGIN
+    DECLARE id INT;
+    INSERT INTO expression (mots, num_Groupe) 
+    VALUES (p_mot, p_numGroupe);
+    SELECT MAX(id_Expression) INTO id FROM expression;
+    INSERT INTO region_expression (num_Region, num_Expression)
+    Values (p_numRegion, id);
+END$$
 
 DROP PROCEDURE IF EXISTS `listeExpressionGroupeRegion`$$
-CREATE DEFINER=`root`@`localhost` PROCEDURE `listeExpressionGroupeRegion` (`p_numGroupe` INT, `p_numRegion` INT)  SELECT id_Expression, mots, num_Groupe, num_Region 
-    FROM expression 
-    WHERE num_Groupe = p_numRegion && num_Region = p_numRegion$$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `listeExpressionGroupeRegion` (`p_numGroupe` INT, `p_numRegion` INT)  SELECT id_expression, mots, num_Groupe, num_Region 
+    FROM expression AS e INNER JOIN region_expression AS re ON e.id_Expression = re.num_Expression
+    WHERE num_Groupe = p_numGroupe && num_Region = p_numRegion$$
+
+DROP PROCEDURE IF EXISTS `listeRegionParIdExpression`$$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `listeRegionParIdExpression` (`p_numExpression` INT)  SELECT num_Region
+	FROM region_expression
+	Where num_Expression = p_numExpression$$
 
 DROP PROCEDURE IF EXISTS `selectExpressionParGroup`$$
 CREATE DEFINER=`root`@`localhost` PROCEDURE `selectExpressionParGroup` (`p_numGroupe` INT)  SELECT id_Expression, mots, num_Groupe, num_Region 
-    FROM expression 
+    FROM expression INNER JOIN region_expression ON id_Expression = num_Expression
     WHERE num_Groupe = p_numGroupe$$
 
 DROP PROCEDURE IF EXISTS `selectExpressionParGroupEtRegion`$$
 CREATE DEFINER=`root`@`localhost` PROCEDURE `selectExpressionParGroupEtRegion` (`p_numGroupe` INT, `p_numRegion` INT)  SELECT id_Expression, mots, num_Groupe, num_Region 
-    FROM expression 
+    FROM expression INNER JOIN region_expression ON id_Expression = num_Expression
     WHERE num_Groupe = p_numGroupe && num_Region = p_numRegion$$
 
 DROP PROCEDURE IF EXISTS `selectExpressionParId`$$
-CREATE DEFINER=`root`@`localhost` PROCEDURE `selectExpressionParId` (`p_id` INT)  SELECT id_Expression, mots, num_Groupe, num_Region 
-    FROM expression 
-    WHERE id_Expression = p_id$$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `selectExpressionParId` (`p_id` INT)  SELECT id_expression, mots, num_Groupe, num_Region 
+    FROM expression INNER JOIN region_expression ON id_Expression = num_Expression
+    WHERE id_expression = p_id$$
+
+DROP PROCEDURE IF EXISTS `selectExpressionsByExpression`$$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `selectExpressionsByExpression` (`p_mot` VARCHAR(255))  SELECT mots FROM expression WHERE num_Groupe = ( SELECT num_Groupe FROM expression WHERE mots = p_mot LIMIT 1)$$
 
 DROP PROCEDURE IF EXISTS `selectGroupe`$$
 CREATE DEFINER=`root`@`localhost` PROCEDURE `selectGroupe` ()  SELECT *
@@ -57,19 +71,14 @@ CREATE DEFINER=`root`@`localhost` PROCEDURE `selectGroupe` ()  SELECT *
     ORDER BY mots_Ref$$
 
 DROP PROCEDURE IF EXISTS `selectGroupeExpression`$$
-CREATE DEFINER=`root`@`localhost` PROCEDURE `selectGroupeExpression` ()  SELECT * 
-    FROM expression 
+CREATE DEFINER=`root`@`localhost` PROCEDURE `selectGroupeExpression` ()  SELECT id_expression, mots, num_Groupe, num_Region
+    FROM expression INNER JOIN region_expression ON id_Expression = num_Expression
     ORDER BY num_Groupe$$
 
 DROP PROCEDURE IF EXISTS `selectRegions`$$
 CREATE DEFINER=`root`@`localhost` PROCEDURE `selectRegions` ()  SELECT id_Region, nom
     FROM region 
     ORDER BY nom$$
-
-DROP PROCEDURE IF EXISTS `updateExpressionParId`$$
-CREATE DEFINER=`root`@`localhost` PROCEDURE `updateExpressionParId` (`p_id` INT, `p_mots` VARCHAR(255), `p_numRegion` INT, `p_numGroupe` INT)  UPDATE expression 
-    SET mots = p_mots, num_Groupe = p_numGroupe, num_Region = p_numRegion 
-    WHERE id_Expression = p_id$$
 
 DELIMITER ;
 
@@ -84,33 +93,31 @@ CREATE TABLE IF NOT EXISTS `expression` (
   `id_Expression` int(11) NOT NULL AUTO_INCREMENT,
   `mots` varchar(255) COLLATE latin1_bin NOT NULL,
   `num_Groupe` int(11) NOT NULL,
-  `num_Region` int(11) NOT NULL,
   PRIMARY KEY (`id_Expression`),
-  KEY `fk_numRegion_Region_idRegion` (`num_Region`),
-  KEY `fk_numGroup_Groupe_idGroup` (`num_Groupe`) USING BTREE
-) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=latin1 COLLATE=latin1_bin;
+  KEY `fk_numGroup_Groupe_idGroup` (`num_Groupe`)
+) ENGINE=InnoDB AUTO_INCREMENT=24 DEFAULT CHARSET=latin1 COLLATE=latin1_bin;
 
 --
 -- Déchargement des données de la table `expression`
 --
 
-INSERT INTO `expression` (`id_Expression`, `mots`, `num_Groupe`, `num_Region`) VALUES
-(1, 'Serpillère', 1, 1),
-(2, 'Toile', 1, 2),
-(3, 'Wassingue', 1, 3),
-(4, 'Petit pain au chocolat', 2, 1),
-(5, 'Pain au chocolat', 2, 2),
-(6, 'Pain au chocolat', 2, 3),
-(7, 'Pain au chocolat', 2, 4),
-(8, 'Pain au chocolat', 2, 5),
-(9, 'Pain au chocolat', 2, 6),
-(10, 'Pain au chocolat', 2, 7),
-(11, 'Pain au chocolat', 2, 8),
-(12, 'Chocolatine', 2, 9),
-(13, 'Pain au chocolat', 2, 10),
-(14, 'Chocolatine', 2, 11),
-(15, 'Pain au chocolat', 2, 12),
-(16, 'Pain au chocolat', 2, 13);
+INSERT INTO `expression` (`id_Expression`, `mots`, `num_Groupe`) VALUES
+(1, 'Serpillère', 1),
+(2, 'toiles3', 1),
+(3, 'Wassingue', 1),
+(4, 'Petit pain au chocolat', 2),
+(5, 'Pain au chocolat', 2),
+(6, 'Pain au chocolat', 2),
+(7, 'Pain au chocolat', 2),
+(8, 'Pain au chocolat', 2),
+(9, 'Pain au chocolat', 2),
+(10, 'Pain au chocolat', 2),
+(11, 'Pain au chocolat', 2),
+(12, 'Chocolatine', 2),
+(13, 'Pain au chocolat', 2),
+(14, 'Chocolatine', 2),
+(15, 'Pain au chocolat', 2),
+(16, 'Pain au chocolat', 2);
 
 -- --------------------------------------------------------
 
@@ -172,6 +179,28 @@ INSERT INTO `region` (`id_Region`, `nom`) VALUES
 (12, 'Provence-Alpes-Cote-d-Azur'),
 (13, 'Corse');
 
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `region_expression`
+--
+
+DROP TABLE IF EXISTS `region_expression`;
+CREATE TABLE IF NOT EXISTS `region_expression` (
+  `num_Region` int(11) NOT NULL,
+  `num_Expression` int(11) NOT NULL,
+  PRIMARY KEY (`num_Region`,`num_Expression`),
+  KEY `FK_numExpression_Expression_idExpression` (`num_Expression`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_bin;
+
+--
+-- Déchargement des données de la table `region_expression`
+--
+
+INSERT INTO `region_expression` (`num_Region`, `num_Expression`) VALUES
+(1, 1),
+(1, 2);
+
 --
 -- Contraintes pour les tables déchargées
 --
@@ -180,8 +209,14 @@ INSERT INTO `region` (`id_Region`, `nom`) VALUES
 -- Contraintes pour la table `expression`
 --
 ALTER TABLE `expression`
-  ADD CONSTRAINT `expression_ibfk_1` FOREIGN KEY (`num_Groupe`) REFERENCES `groupe` (`id_Groupe`),
-  ADD CONSTRAINT `fk_numRegion_Region_idRegion` FOREIGN KEY (`num_Region`) REFERENCES `region` (`id_Region`);
+  ADD CONSTRAINT `fk_numGroup_Groupe_idGroup` FOREIGN KEY (`num_Groupe`) REFERENCES `groupe` (`id_Groupe`);
+
+--
+-- Contraintes pour la table `region_expression`
+--
+ALTER TABLE `region_expression`
+  ADD CONSTRAINT `FK_numExpression_Expression_idExpression` FOREIGN KEY (`num_Expression`) REFERENCES `expression` (`id_Expression`),
+  ADD CONSTRAINT `FK_numRegion_Region_idREgion` FOREIGN KEY (`num_Region`) REFERENCES `region` (`id_Region`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
--- BD/procedureStock.sql
+++ BD/procedureStock.sql
@@ -4,42 +4,49 @@ Use BDD_Expression; -- utilise la BDD
 DROP PROCEDURE IF EXISTS listeExpressionGroupeRegion; -- Supprime si elle existe déjà
 CREATE PROCEDURE listeExpressionGroupeRegion (p_numGroupe INT, p_numRegion INT) 
     SELECT id_expression, mots, num_Groupe, num_Region 
-    FROM expression 
-    WHERE num_Groupe = p_numRegion && num_Region = p_numRegion;
+    FROM expression AS e INNER JOIN region_expression AS re ON e.id_Expression = re.num_Expression
+    WHERE num_Groupe = p_numGroupe && num_Region = p_numRegion;
 
 
 DROP PROCEDURE IF EXISTS ajoutExpression; -- Supprime si elle existe déjà
+DELIMITER |
 CREATE PROCEDURE ajoutExpression (p_mot varchar(255), p_numGroupe INT, p_numRegion INT) 
-    INSERT INTO expression (mot, num_Groupe, num_Region) 
-    VALUES (p_mot, p_numGroupe, p_numRegion);
+BEGIN
+    DECLARE id INT;
+    INSERT INTO expression (mots, num_Groupe) 
+    VALUES (p_mot, p_numGroupe);
+    SELECT MAX(id_Expression) INTO id FROM expression;
+    INSERT INTO region_expression (num_Region, num_Expression)
+    Values (p_numRegion, id);
+END |
 
 
 DROP PROCEDURE IF EXISTS selectExpressionParId; -- Supprime si elle existe déjà
 CREATE PROCEDURE selectExpressionParId (p_id INT) 
     SELECT id_expression, mots, num_Groupe, num_Region 
-    FROM expression 
+    FROM expression INNER JOIN region_expression ON id_Expression = num_Expression
     WHERE id_expression = p_id;
 
 
 DROP PROCEDURE IF EXISTS selectGroupeExpression; -- Supprime si elle existe déjà
 CREATE PROCEDURE selectGroupeExpression()
     SELECT id_expression, mots, num_Groupe, num_Region
-    FROM expression 
+    FROM expression INNER JOIN region_expression ON id_Expression = num_Expression
     ORDER BY num_Groupe;
 
-
-DROP PROCEDURE IF EXISTS selectExpressionParGroup; -- Supprime si elle existe déjà
-CREATE PROCEDURE selectExpressionParGroup()
-    SELECT id_expression, mots, num_Groupe, num_Region 
-    FROM expression 
-    WHERE num_Groupe = p_numGroupe;
-
-
-DROP PROCEDURE IF EXISTS updateExpressionParId; -- Supprime si elle existe déjà
+-- Mis de côté
+/*DROP PROCEDURE IF EXISTS updateExpressionParId; -- Supprime si elle existe déjà
+DELIMITER |
 CREATE PROCEDURE updateExpressionParId(p_id INT, p_mots varchar(255), p_numRegion INT, p_numGroupe INT) 
+BEGIN
     UPDATE expression 
-    SET mots = p_mots, num_Groupe = p_numGroupe, num_Region = p_numRegion 
+    SET mots = p_mots, num_Groupe = p_numGroupe 
     WHERE id_expression = p_id;
+
+    UPDATE region_expression
+    SET num_Region = p_numRegion
+    WHERE num_Expression = p_id;
+END |*/
 
 DROP PROCEDURE IF EXISTS selectRegions; -- Supprime si elle existe déjà
 CREATE PROCEDURE selectRegions()
@@ -47,22 +54,17 @@ CREATE PROCEDURE selectRegions()
     FROM region 
     ORDER BY nom;
 
-DROP PROCEDURE IF EXISTS selectGroupeExpression; -- Supprime si elle existe déjà
-CREATE PROCEDURE selectGroupeExpression()
-    SELECT * 
-    FROM expression 
-    ORDER BY num_Groupe;
 
 DROP PROCEDURE IF EXISTS selectExpressionParGroup; -- Supprime si elle existe déjà
 CREATE PROCEDURE selectExpressionParGroup(p_numGroupe INT)
     SELECT id_Expression, mots, num_Groupe, num_Region 
-    FROM expression 
+    FROM expression INNER JOIN region_expression ON id_Expression = num_Expression
     WHERE num_Groupe = p_numGroupe;
 
 DROP PROCEDURE IF EXISTS selectExpressionParGroupEtRegion; -- Supprime si elle existe déjà
 CREATE PROCEDURE selectExpressionParGroupEtRegion(p_numGroupe INT,p_numRegion INT)
     SELECT id_Expression, mots, num_Groupe, num_Region 
-    FROM expression 
+    FROM expression INNER JOIN region_expression ON id_Expression = num_Expression
     WHERE num_Groupe = p_numGroupe && num_Region = p_numRegion;
     
 DROP PROCEDURE IF EXISTS selectGroupe; -- Supprime si elle existe déjà
@@ -70,4 +72,8 @@ CREATE PROCEDURE selectGroupe()
     SELECT *
     FROM groupe 
     ORDER BY mots_Ref;
+
+DROP PROCEDURE IF EXISTS selectExpressionsByExpression; -- Supprime si elle existe déjà
+CREATE PROCEDURE selectExpressionsByExpression(p_mot varchar(255))
+    SELECT mots FROM expression WHERE num_Groupe = ( SELECT num_Groupe FROM expression WHERE mots = p_mot LIMIT 1);
 
